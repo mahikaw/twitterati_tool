554c66b06c600bd9a9bf64fff768bc86770ae9a6
mahikaw <mahika16241@iiitd.ac.in>
2017-08-14 08:38:05 +0530

    make it better

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from pymongo import MongoClient
+import json
 client = MongoClient()
 db = client.tweets
 modi=db.narendra_modi
@@ -9,33 +10,60 @@ app = Flask(__name__)
 
 modi_tweet_locations={}
 modi_hastgas={}
+trending={}
 retweet_modi_count=0
 tweet_modi_count=0
 favorite_count=0
+image_count=0
+video_count=0
+text_count=0
 for i in modi.find({}):
 
 	tweet_modi_count+=1
+	if "media" in i['entities']:
+		if i['entities']['media'][0]['type']=="photo":
+			image_count+=1
+		elif	i['entities']['media'][0]['type']=="video":
+			video_count+=1
+	if "extended_tweet" in i:
+		print "mahika mahika mahika mahika mahika mahika"
+		print i
+		if "media" in i['extended_tweet']:
+			if i['extended_tweet']['media'][0]['type']=="photo":
+				image_count+=1
+			elif	i['entities']['media'][0]['type']=="video":
+				video_count+=1
+	else:
+		text_count+=1
+	if "retweeted_status" in i:
+		retweet_modi_count+=i['retweeted_status']['retweet_count']
 
-	retweet_modi_count+=i['retweet_count']
+		favorite_count=favorite_count+i['retweeted_status']['favorite_count']
 
-	favorite_count=favorite_count+i['favorite_count']
+	if i['user']['time_zone'] not in modi_tweet_locations and i['user']['time_zone']!="null":
 
-	if i['user']['time_zone'] not in modi_tweet_locations:
-		# if(i['user']['time_zone'])=='Eastern Time (US & Canada)':
-		# 	modi_tweet_locations['Unites states']=1
-		# else:
 		modi_tweet_locations[i['user']['time_zone']]=1
 	else:
 		modi_tweet_locations[i['user']['time_zone']]=modi_tweet_locations[i['user']['time_zone']]+1
-
-	# print("entities data\n"+str(i['entities']))
-	# print("^^^^^^^^"+i['entities']['hashtags'][0]['text'])
-	# if i['entities']['hashtags'] not in modi_tweet_locations:
-	# 	 modi_tweet_locations[i['user']['time_zone']]=1
-	# else:
-	# 	modi_tweet_locations[i['user']['time_zone']]=modi_tweet_locations[i['user']['time_zone']]+1
-
-
+	
+	if i['entities']['hashtags']!=[]:
+		access_hash=i['entities']['hashtags'][0]['text']
+		print access_hash
+		if access_hash not in trending:
+			trending[access_hash]=1
+		else:
+			trending[access_hash]=trending[access_hash]+1
+print trending
+c=sorted(trending, key=trending.get,reverse=True)
+d=[]
+flag=0
+i=0
+while(flag!=10):
+	i+=1
+	if(c[i][0]!='\\'):
+		flag+=1
+		d.append(c[i])
+print d
 print("LOCATIONS"+str(modi_tweet_locations))
 
 arvind_tweet_locations={}
@@ -43,7 +71,7 @@ retweet_arvind_count=0
 tweet_arvind_count=0
 for j in kejriwal.find({}):
 	tweet_arvind_count+=1
-	retweet_arvind_count+=i['retweet_count']
+	# retweet_arvind_count+=i['retweet_count']
 	favorite_count=favorite_count+j['favorite_count']
 	if j['user']['time_zone'] not in arvind_tweet_locations:
 		arvind_tweet_locations[j['user']['time_zone']]=1
@@ -54,12 +82,12 @@ print("arvind count"+str(tweet_arvind_count))
 print(">>>>>>>"+str(tweets_Total))
 retweet_total=retweet_modi_count+retweet_arvind_count
 print(">>>>>>>"+str(retweet_total))
-
+# print modi_tweet_locations
 
 @app.route('/')
 def hello_world():
     return render_template('index.html',tweet_arvind_count=tweet_arvind_count,tweet_modi_count=tweet_modi_count,
-    	tweets_Total=tweets_Total,retweet_total=retweet_total,favorite_count=favorite_count,m_location=modi_tweet_locations)
+    	tweets_Total=tweets_Total,retweet_total=retweet_total,favorite_count=favorite_count,m_location=json.dumps(modi_tweet_locations),trending=d,image_count=image_count,text_count=text_count,video_count=video_count)
 
 if __name__ == '__main__':
 	app.run(debug=True,use_reloader=True)
