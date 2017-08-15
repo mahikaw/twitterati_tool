824d61e814e7b441d04f652e1f7aae2d18e182b7
mahikaw <mahika16241@iiitd.ac.in>
2017-08-15 11:50:28 +0530

    update

--- test.py
+++ test.py
@@ -57,25 +57,51 @@ i=0
 arvind_tweet_locations={}
 retweet_arvind_count=0
 tweet_arvind_count=0
-for j in kejriwal.find({}):
-        tweet_arvind_count+=1
-        # retweet_arvind_count+=i['retweet_count']
-        favorite_count=favorite_count+j['favorite_count']
-        if j['user']['time_zone'] not in arvind_tweet_locations:
-                arvind_tweet_locations[j['user']['time_zone']]=1
-        else:
-                arvind_tweet_locations[j['user']['time_zone']]=arvind_tweet_locations[j['user']['time_zone']]+1
-tweets_Total=tweet_arvind_count+tweet_modi_count
-print("arvind count"+str(tweet_arvind_count))
-print(">>>>>>>"+str(tweets_Total))
-retweet_total=retweet_modi_count+retweet_arvind_count
-print(">>>>>>>"+str(retweet_total))
-# print modi_tweet_locations
 
+
+
+
+for i in kejriwal.find({}):
+
+        tweet_arvind_count+=1
+        if "media" in i['entities']:
+                if i['entities']['media'][0]['type']=="photo":
+                        image_count+=1
+                elif    i['entities']['media'][0]['type']=="video":
+                        video_count+=1
+        if "extended_tweet" in i:
+                if "media" in i['extended_tweet']:
+                        if i['extended_tweet']['media'][0]['type']=="photo":
+                                image_count+=1
+                        elif    i['entities']['media'][0]['type']=="video":
+                                video_count+=1
+        else:
+                text_count+=1
+        if "retweeted_status" in i:
+                retweet_arvind_count+=i['retweeted_status']['retweet_count']
+
+                favorite_count=favorite_count+i['retweeted_status']['favorite_count']
+
+        if i['user']['time_zone'] not in arvind_tweet_locations and i['user']['time_zone']!="null":
+
+                arvind_tweet_locations[i['user']['time_zone']]=1
+        else:
+                arvind_tweet_locations[i['user']['time_zone']]=arvind_tweet_locations[i['user']['time_zone']]+1
+
+        if i['entities']['hashtags']!=[]:
+                access_hash=i['entities']['hashtags'][0]['text']
+                if access_hash not in trending:
+                        trending[access_hash]=1
+                else:
+                        trending[access_hash]=trending[access_hash]+1
+tweets_Total=tweet_arvind_count+tweet_modi_count
+
+retweet_total=retweet_modi_count+retweet_arvind_count
+print(">>>>>>"+str(retweet_arvind_count)+"  "+str(tweet_arvind_count))
 @app.route('/')
 def hello_world():
     return render_template('index.html',tweet_arvind_count=tweet_arvind_count,tweet_modi_count=tweet_modi_count,
-        tweets_Total=tweets_Total,retweet_total=retweet_total,favorite_count=favorite_count,m_location=json.dumps(modi_tweet_locations),trending=d,image_count=image_count,text_count=text_count)
+        tweets_Total=tweets_Total,retweet_total=retweet_total,favorite_count=favorite_count,m_location=json.dumps(modi_tweet_locations),trending=d,image_count=image_count,text_count=text_count,video_count=video_count)
 
 if __name__ == '__main__':
         app.run(debug=True,use_reloader=True)
